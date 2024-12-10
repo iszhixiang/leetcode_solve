de5b9f9c8c4fa694f124ec9425de0e2a14663799
小李同学 <126527815+iszhixiang@users.noreply.github.com>
2024-12-10 18:04:32 +0800

    Add files via upload

---
+++ leetcode_2874有序三元组中的最大值.java
@@ -0,0 +1,26 @@
+class Solution {
+    public long maximumTripletValue(int[] nums) {
+        //将三个变量的枚举减少到1个变量的枚举
+        int len = nums.length;
+        int []shuzu = new int[len+1];//记录后面元素的最大值
+
+        for(int i=len-1;i>1;i--){
+            shuzu[i] = Math.max(shuzu[i+1],nums[i]);
+        }
+        int tempmax =nums[0];
+        long maxsum = 0;
+        for(int i=1;i<len-1;i++){
+            maxsum = Math.max(maxsum,(long)(tempmax-nums[i])*shuzu[i+1]);
+            tempmax = Math.max(tempmax,nums[i]);
+        }
+        return maxsum;
+    }
+}
+/*
+ * 定义数组倒序遍历，shuzu[i] 表示从这个数开始，一直到数组中最后一个数 出现的最大的一个数
+ * shuzu 表示选为 k 的数，k选中你的数要最大，得到的结果才会最大
+ * 第一个数也是最大的，可以用一个变量，不断的更新最大值，
+ * 中间值j 就循环遍历中间的值，选中最小的更新即可。
+ * 
+ * 
+ */
