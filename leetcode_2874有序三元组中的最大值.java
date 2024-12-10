88c2f33181738a889392712e02ced77594bf53bd
小李同学 <126527815+iszhixiang@users.noreply.github.com>
2024-12-10 18:07:11 +0800

    Update leetcode_2874有序三元组中的最大值.java

--- leetcode_2874有序三元组中的最大值.java
+++ leetcode_2874有序三元组中的最大值.java
@@ -1,3 +1,13 @@
+/*
+给你一个下标从 0 开始的整数数组 nums 。
+
+请你从所有满足 i < j < k 的下标三元组 (i, j, k) 中，找出并返回下标三元组的最大值。如果所有满足条件的三元组的值都是负数，则返回 0 。
+
+下标三元组 (i, j, k) 的值等于 (nums[i] - nums[j]) * nums[k] 。
+
+ 
+*/
+
 class Solution {
     public long maximumTripletValue(int[] nums) {
         //将三个变量的枚举减少到1个变量的枚举
